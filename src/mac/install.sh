928164455fdeaddc860f9557970249bc8b2dec53
Bálint Séra <balint.sera@gmail.com>
2020-05-05 12:08:47 +0200

    Quick fix hard coded composer checksum to dynamic

--- src/mac/install.sh
+++ src/mac/install.sh
@@ -67,10 +67,18 @@ fi
 if [ "$COMPOSER_INSTALL" == "1" ]; then
     echo -e "Installing Composer..."
     
+    EXPECTED_CHECKSUM="$(curl -s https://composer.github.io/installer.sig)"
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    php -r "if (hash_file('sha384', 'composer-setup.php') === 'c5b9b6d368201a9db6f74e2611495f369991b72d9c8cbd3ffbc63edff210eb73d46ffbfce88669ad33695ef77dc76976') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-    php composer-setup.php
-    php -r "unlink('composer-setup.php');"
+    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+    then
+      >&2 echo 'ERROR: Invalid installer checksum'
+      rm composer-setup.php
+    fi
+
+    php composer-setup.php --quiet
+    rm composer-setup.php
 
     mv composer.phar /usr/local/bin/composer
     composer self-update
